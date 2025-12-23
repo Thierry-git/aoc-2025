5ad4d08e59b74e8fd32894102ff19c1d860ab8af
Thierry-git <thierry.poulin000@gmail.com>
2025-12-22 21:24:04 -0500

    feat(day6): declared basic structures and classes for a producer-consumer scheme

--- day6/solution.h
+++ day6/solution.h
@@ -2,6 +2,9 @@
 
 #include "../common/aoc.h"
 
+#include <mutex>
+#include <semaphore>
+
 namespace solution {
 
 // ============================================================================
@@ -9,6 +12,59 @@ namespace solution {
 // ============================================================================
 
 using Result = long;
+
+using Operand = int;
+enum class Operation {
+    Times = '*',
+    Plus = '+',
+};
+
+static constexpr int PROBLEM_LENGTH = 4;
+struct Problem {
+    Problem() = delete;
+    constexpr Problem() : operands {}, op(Operation::Times), isSentinel(false) { }
+
+    std::array<Operand, PROBLEM_LENGTH> operands;
+    Operation op;
+
+private:
+    bool isSentinel = false;
+
+    friend class ProblemsMonitor;
+
+    constexpr Problem() : operands({}), op(Operation::Times), isSentinel(true) { }
+};
+
+class ProblemsMonitor {
+public:
+    ProblemsMonitor() = default;
+
+    void push_back(const Problem& problem);
+    Problem pop_front();
+
+    void pushSentinel() { push_back(SENTINEL); }
+
+private:
+    static constexpr int PROBLEM_BUFSIZE = 16;
+    std::array<Problem, PROBLEM_BUFSIZE> problems_;
+    std::counting_semaphore<PROBLEM_BUFSIZE> empty_ { PROBLEM_BUFSIZE };
+    std::counting_semaphore<PROBLEM_BUFSIZE> full_ { 0 };
+    std::mutex mtx_;
+
+    static constexpr Problem SENTINEL = {};
+};
+
+class ResultMonitor {
+public:
+    ResultMonitor() = default;
+
+    void add(Result qty);
+    Result get();
+
+private:
+    Result result_ = 0;
+    std::mutex mtx_;
+};
 
 /**
  * @brief Base class for Day 6.
@@ -22,13 +78,11 @@ public:
     Result solve() const override;
 
 protected:
-    // TODO: Add shared helper methods here
+    static constexpr int NUM_PRODUCERS = 2;
+    static constexpr int NUM_CONSUMERS = 2;
 
-    /**
-     * @brief Process a single item from the input.
-     * Override in Part1/Part2 with different logic.
-     */
-    // virtual long processItem(...) const = 0;
+    virtual void producer() const = 0;
+    virtual void consumer() const = 0;
 };
 
 class Day6Part1 : public Day6 {
@@ -36,7 +90,8 @@ public:
     explicit Day6Part1(const std::string& inputFile) : Day6(inputFile) { }
 
 protected:
-    // TODO: Override methods as needed
+    virtual void producer() const override;
+    virtual void consumer() const override;
 };
 
 class Day6Part2 : public Day6 {
@@ -44,7 +99,8 @@ public:
     explicit Day6Part2(const std::string& inputFile) : Day6(inputFile) { }
 
 protected:
-    // TODO: Override methods as needed
+    virtual void producer() const override;
+    virtual void consumer() const override;
 };
 
 using Day6Part1Test = aoc::TestDecorator<Day6Part1>;
